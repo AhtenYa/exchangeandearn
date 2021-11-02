a879039ce9d607f019b2fe5be55c38fc11ec6c16
Ahten Ya <68775649+AhtenYa@users.noreply.github.com>
2021-11-02 17:28:41 +0100

    Added templates for user's settings.

--- exchange/urls.py
+++ exchange/urls.py
@@ -5,9 +5,11 @@ from . import views
 app_name = 'exchange'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('profile/logout', views.logout_view, name='logout'),
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
     path('login/', views.UserLoginView.as_view(), name='user_login'),
-    path('index/', views.UserIndexView.as_view(), name='user_index'),
-    path('index/settings', views.UserSettingsView.as_view(), name='user_settings'),
-    path('index/logout', views.logout_view, name='logout'),
+    path('profile/', views.UserIndexView.as_view(), name='user_index'),
+    path('profile/settings', views.UserSettingsView.as_view(), name='user_settings'),
+    path('profile/password', views.UserPasswordView.as_view(), name='user_password'),
+    path('profile/delete/<slug:slug>', views.UserDeleteView.as_view(), name='user_delete'),
 ]
--- exchange/views.py
+++ exchange/views.py
@@ -16,6 +16,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from django.contrib.auth.models import User
 
+from stock.models import Currency
 from .models import Account, Order, Transfer
 from .forms import LoginForm, RegisterForm, PasswordForm
 
@@ -27,11 +28,47 @@ class IndexView(DetailView):
         return render(request, 'exchange/index.html')
 
 
+@method_decorator(login_required, name='dispatch')
 class UserIndexView(DetailView):
     template_name = 'exchange/user_index.html'
 
     def get(self, request):
-        return render(request, 'exchange/user_index.html')
+        context = {}
+        account_info = {}
+
+        account = Account.objects.get(owner=request.user)
+
+        account_info['status'] = account.status
+        account_info['currency'] = account.currency
+        account_info['balance'] = account.balance
+
+        context['account_info'] = account_info
+
+        return render(request, 'exchange/user_index.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class UserPasswordView(PermissionRequiredMixin, PasswordChangeView):
+    permission_required = 'auth.change_user'
+    template_name = 'exchange/user_password.html'
+    form_class = PasswordForm
+    success_url = reverse_lazy('exchange:user_settings')
+
+
+@method_decorator(login_required, name='dispatch')
+class UserDeleteView(PermissionRequiredMixin, DeleteView):
+    permission_required = 'auth.delete_user'
+    template_name = 'exchange/user_delete.html'
+    model = User
+    success_url = reverse_lazy('stock:index')
+    slug_field = "username"
+
+    def has_permission(self):
+        if self.request.user == self.get_object():
+            perms = self.get_permission_required()
+            return self.request.user.has_perms(perms)
+        else:
+            return False
 
 
 @method_decorator(login_required, name='dispatch')
